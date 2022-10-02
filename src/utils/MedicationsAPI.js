4c1ea73957bb6366c7235928113c936ff48af8dd
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-10-02 17:06:21 +0200

    add Medications API

---
+++ src/utils/MedicationsAPI.js
@@ -0,0 +1,15 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || "http://run.mocky.io";
+
+let token = localStorage.token;
+
+if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+
+const headers = {
+  Accept: "application/json",
+  Authorization: token,
+};
+
+export const getAll = () =>
+  fetch(`${api}/v3/a78af611-3af6-481c-98a0-ba04a5f7ce70`, { headers })
+    .then((res) => res.json())
+    .then((data) => data.books);
